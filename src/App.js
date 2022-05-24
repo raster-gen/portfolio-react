8c806c98e88bcacbbc271ed79dbf65e90e27b334
raster-gen <rasterroot@gmail.com>
2022-05-24 18:57:04 +0100

    add clear components to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
 import About from "./components/about/About";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import Portfolio from "./components/portfolio/Portfolio";
+import Services from "./components/services/Services";
+import Experience from "./components/experience/Experience";
+import Testimonials from "./components/testimonials/Testimonials";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Nav />
       <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
     </div>
   );
 }
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./Testimonials.module.css";
 
 const Testimonials = () => {
-  return <div></div>;
+  return <div>Testimonials</div>;
 };
 
 export default Testimonials;
