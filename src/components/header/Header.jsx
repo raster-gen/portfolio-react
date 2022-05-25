687594bae8a2a9cda02e09b32b078fc0794971f7
raster-gen <rasterroot@gmail.com>
2022-05-25 14:50:30 +0100

    add social components for header and mount react-icon library

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import classes from "./Header.module.css";
+import CTA from "./CTA";
+import myPhoto from "../../assets/me.png";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello, I'm</h5>
+        <h1>Gennady Raster</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={myPhoto} alt="me" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
