8e7bd60b735e46d2d630e9f7feda344d53c5e423
raster-gen <rasterroot@gmail.com>
2022-06-01 13:54:11 +0100

    set up sending messages from the form

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import emailjs from 'emailjs-com';
+
 import './contactForm.css'
 
 const ContactForm = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_zjy93cc', 'template_0qct16k', form.current, 'sUtHkBr9GOHxthtNL')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
     return (
-        <form className={'contact__form'}>
+        <form ref={form} onSubmit={sendEmail} className={'contact__form'}>
             <input type="text" name='name' placeholder={'Your Full Name'} required/>
             <input type="email" name='email' placeholder={'Your Email'} required/>
             <textarea name="message" rows="7" placeholder={'Your Message...'} required></textarea>
-            <button type={'submit'}  className={'btn btn-primary'}>Send Message</button>
+            <button type={'submit'} value={'Send'} className={'btn btn-primary'}>Send Message</button>
         </form>
     );
 };
